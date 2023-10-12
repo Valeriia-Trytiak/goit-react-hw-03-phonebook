9530e20c891563d1702d314a0390e83ea90befd8
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-10-13 01:12:44 +0200

    update tack-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,31 @@ import { nanoid } from 'nanoid'
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
+import { Container } from "./Container/Container.styled";
 
 export class App extends Component {
 
   state = {
-    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: ''
   }
+
+  //При завантаженні сторінки беру дані з локального сховища для відмальовки
+componentDidMount(){
+const saveContacts = localStorage.getItem('contacts-list');
+if (saveContacts !==null) {
+  this.setState({ contacts: JSON.parse(saveContacts) });
+}
+  }
+
+  //Записую у локальне сховище дані зі стейту
+componentDidUpdate(prevProps, prevState){
+if (prevState.contacts !== this.state.contacts) {
+  localStorage.setItem('contacts-list', JSON.stringify(this.state.contacts))
+}
+
+}
+
 
 //Додаю контакт до стану
 addNewContact = (newContact) => {
@@ -51,12 +66,12 @@ this.setState(prevState => ({
       contact.name.toLowerCase().includes(filter.toLowerCase())
     ) : contacts;
 
-    return <div>
+    return <Container>
    <h1>Phonebook</h1>
    <ContactForm onUpdateContactList={this.addNewContact} />
    <h2>Contacts</h2>
    <Filter onSearchContact={this.uppdateFilter} filterName ={filter} />
   <ContactList users= {visibleContacts} onDeleteContact={this.deleteContact} />
-    </div>
+    </Container>
   }
 };
